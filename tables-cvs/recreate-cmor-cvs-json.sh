88cf6e8ccc46b208ea0a2075f0629b0b7d70de76
znichollscr <114576287+znichollscr@users.noreply.github.com>
2026-04-21 19:17:03 +1000

    Add missing subhr frequency and validation info (#68)
    * Move script into this repo
    
    * Update script
    
    * Fix up deps
    
    * Add approx_interval_* info
    
    * Use branches that have subhr
    
    * Add validation of values
    
    * Update CMOR CVs JSON via GitHub action
    
    * Move to esgvoc tagged version
    
    * Fix reviewer of auto-generated PRs
    
    ---------
    
    Co-authored-by: znichollscr <ci-runner@cmip7-cmor-tables.invalid>

--- tables-cvs/recreate-cmor-cvs-json.sh
+++ tables-cvs/recreate-cmor-cvs-json.sh
@@ -18,13 +18,11 @@
 # Environment variables that this file uses.
 # If they're not set, the default values are used.
 ESGVOC_FORK="${ESGVOC_FORK:=ESGF}"
-ESGVOC_REVISION="${ESGVOC_REVISION:=8d5e7fb}"
-# ESGVOC_FORK="${ESGVOC_FORK:=ESGF}"
-# ESGVOC_REVISION="${ESGVOC_REVISION:=9fcf732}" # v3.0.0
+ESGVOC_REVISION="${ESGVOC_REVISION:=cd1ad4d}" # v3.1.0
 UNIVERSE_CVS_FORK="${UNIVERSE_CVS_FORK:=WCRP-CMIP}"
-UNIVERSE_CVS_BRANCH="${UNIVERSE_CVS_BRANCH:=esgvoc_dev}"
+UNIVERSE_CVS_BRANCH="${UNIVERSE_CVS_BRANCH:=esgvoc}"
 CMIP7_CVS_FORK="${CMIP7_CVS_FORK:=WCRP-CMIP}"
-CMIP7_CVS_BRANCH="${CMIP7_CVS_BRANCH:=esgvoc_dev}"
+CMIP7_CVS_BRANCH="${CMIP7_CVS_BRANCH:=esgvoc}"
 
 verbose=0
 install_env=0
@@ -67,9 +65,9 @@ if [[ $install_env -eq 1 ]]; then
 
     sed -i -E -e 's#(.*)/github.com/.*/(.*)#\1/github.com/'"${ESGVOC_FORK}"'/\2#' "${requirements_file}"
     sed -i -E -e 's#(.*)/esgf-vocab.git@.*#\1/esgf-vocab.git@'"${ESGVOC_REVISION}"'#' "${requirements_file}"
-    # # Mac equivalent of the above
-    # sed -i -E -e 's#\(.*\)/github.com/.*/\(.*\)#\1/github.com/'"${ESGVOC_FORK}"'/\2#' "${requirements_file}"
-    # sed -i -E -e 's#\(.*\)/esgf-vocab.git@.*#\1/esgf-vocab.git@'"${ESGVOC_REVISION}"'#' "${requirements_file}"
+    # Mac equivalent of the above
+    sed -i -E -e 's#\(.*\)/github.com/.*/\(.*\)#\1/github.com/'"${ESGVOC_FORK}"'/\2#' "${requirements_file}"
+    sed -i -E -e 's#\(.*\)/esgf-vocab.git@.*#\1/esgf-vocab.git@'"${ESGVOC_REVISION}"'#' "${requirements_file}"
 
     pip install -r "${requirements_file}"
 
