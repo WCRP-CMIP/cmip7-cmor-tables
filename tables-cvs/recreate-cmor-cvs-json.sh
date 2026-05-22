7ef1c83813c617ed6efe08845d99aff556633ed0
znichollscr <114576287+znichollscr@users.noreply.github.com>
2026-05-22 23:22:01 +1000

    Update grid and other descriptions (#104)
    * Try switching to base, but broken
    
    * Update esgvoc versions
    
    * Delete redundant comment
    
    * Fix up cmomenting

--- tables-cvs/recreate-cmor-cvs-json.sh
+++ tables-cvs/recreate-cmor-cvs-json.sh
@@ -28,16 +28,16 @@ set -euo pipefail
 # # Use when we are using a branches of CVs
 esgvoc_versioned=0
 ESGVOC_FORK="${ESGVOC_FORK:=ESGF}"
-ESGVOC_REVISION="${ESGVOC_REVISION:=4.0.0}"
-# UNIVERSE_CVS_FORK="${UNIVERSE_CVS_FORK:=znichollscr}"
-# UNIVERSE_CVS_REF="${UNIVERSE_CVS_REF:=update-experiment-definitions}"
+ESGVOC_REVISION="${ESGVOC_REVISION:=4.0.1}"
 UNIVERSE_CVS_FORK="${UNIVERSE_CVS_FORK:=WCRP-CMIP}"
 UNIVERSE_CVS_REF="${UNIVERSE_CVS_REF:=esgvoc_dev}"
+# UNIVERSE_CVS_FORK="${UNIVERSE_CVS_FORK:=znichollscr}"
+# UNIVERSE_CVS_REF="${UNIVERSE_CVS_REF:=update-experiment-definitions}"
 CMIP7_CVS_FORK="${CMIP7_CVS_FORK:=WCRP-CMIP}"
-CMIP7_CVS_REF="${CMIP7_CVS_REF:=latest-fixes}"
-# CMIP7_CVS_REF="${CMIP7_CVS_REF:=esgvoc_dev}"
+CMIP7_CVS_REF="${CMIP7_CVS_REF:=esgvoc_dev}"
+# CMIP7_CVS_REF="${CMIP7_CVS_REF:=latest-fixes}"
 
-## Versioned esgvoc config
+# # Versioned esgvoc config
 # # Use when we are using a versioned esgvoc release
 # esgvoc_versioned=1
 # ESGVOC_FORK="${ESGVOC_FORK:=ESGF}"
